f64f21887ee6fbeff39ae31b46a30fb9aacbcbb7
Jitendra Rautela <jitendra.rautela@citiustech.com>
2020-07-03 21:21:14 +0530

    Display patient conditions
    View patient conditions in tablular form and also sort using column
    header

--- src/app/fhir-form/fhir-form.component.ts
+++ src/app/fhir-form/fhir-form.component.ts
@@ -9,10 +9,13 @@ import { FhirSearchService } from '../service/fhir-search.service';
 export class FhirFormComponent implements OnInit {
 
   private data: any[];
-  private patient: any[] = [];
+  private patients: any[] = [];
   private condition: any[] = [];
   private selectedPatient: any;
   private patientCondition: any = [];
+  private conditionSorter = true;
+  private dateSorter = true;
+  private statusSorter = true;
 
   constructor(private service: FhirSearchService) { }
 
@@ -25,26 +28,27 @@ export class FhirFormComponent implements OnInit {
       this.data = res.entry;
       this.filterData();
       this.selectedPatient = this.data[0].resource;
-      this.selectedPatientDetails();
+      this.selectedPatientCondition();
     });
   }
 
   selectPatient(id: string) {
     this.selectedPatient = this.data.filter(patient => patient.resource.id === id)[0].resource;
-    this.selectedPatientDetails();
+    this.selectedPatientCondition();
   }
 
   filterData() {
     for (const data of this.data) {
       if (data.resource.resourceType === 'Patient') {
-        this.patient.push(data);
+        this.patients.push(data);
       } else {
         this.condition.push(data);
       }
     }
   }
 
-  selectedPatientDetails() {
+  selectedPatientCondition() {
+    this.patientCondition = [];
     for (const condition of this.condition) {
       if (condition.resource.subject.reference === 'Patient/' + this.selectedPatient.id) {
         this.patientCondition.push(condition.resource);
@@ -52,19 +56,45 @@ export class FhirFormComponent implements OnInit {
     }
   }
 
+  getFullName(patient: any): string {
+    if (patient !== undefined) {
+      return patient.name[0].given[0] + ' ' + patient.name[0].family;
+    }
+  }
+
   sortByDate() {
     this.patientCondition.sort((a, b) => {
-      return this.getTime(a.onsetDateTime) - this.getTime(b.onsetDateTime);
+      if (this.dateSorter) {
+        return this.getTime(a.onsetDateTime) - this.getTime(b.onsetDateTime);
+      } else {
+        return this.getTime(b.onsetDateTime) - this.getTime(a.onsetDateTime);
+      }
     });
+    this.dateSorter = !this.dateSorter;
   }
 
   sortByCondition() {
-    this.patientCondition.sort((a, b) => a.code.text < b.code.text ? -1 : a.code.text > b.code.text ? 1 : 0);
+    this.patientCondition.sort((a, b) => {
+      if (this.conditionSorter) {
+        return a.code.text < b.code.text ? -1 : a.code.text > b.code.text ? 1 : 0;
+      } else {
+        return a.code.text < b.code.text ? 1 : a.code.text > b.code.text ? -1 : 0;
+      }
+    });
+    this.conditionSorter = !this.conditionSorter;
   }
 
   sortByStatus() {
-    this.patientCondition.sort((a, b) => a.clinicalStatus.coding[0].code < b.clinicalStatus.coding[0].code 
-            ? -1 : a.clinicalStatus.coding[0].code > b.clinicalStatus.coding[0].code ? 1 : 0);
+    this.patientCondition.sort((a, b) => {
+      if (this.statusSorter) {
+        return a.clinicalStatus.coding[0].code < b.clinicalStatus.coding[0].code
+        ? -1 : a.clinicalStatus.coding[0].code > b.clinicalStatus.coding[0].code ? 1 : 0;
+      } else {
+        return a.clinicalStatus.coding[0].code < b.clinicalStatus.coding[0].code
+        ? 1 : a.clinicalStatus.coding[0].code > b.clinicalStatus.coding[0].code ? -1 : 0;
+      }
+    });
+    this.statusSorter = !this.statusSorter;
   }
 
   private getTime(date?: Date) {
